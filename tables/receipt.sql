fd4867723d9c1ac90782c2cb2d38ee606d571be3
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-04-29 13:37:26 +0200

    just reformatting code

--- tables/receipt.sql
+++ tables/receipt.sql
@@ -5,7 +5,7 @@ CREATE TABLE receipt
     id_shop       INT  NOT NULL,
     id_product    INT  NOT NULL,
     id_client     INT  NOT NULL,
-    id_employee    INT  NOT NULL,
+    id_employee   INT  NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_shop) REFERENCES shop (id),
     FOREIGN KEY (id_product) REFERENCES product (id),
